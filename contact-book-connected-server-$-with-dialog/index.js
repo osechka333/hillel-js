60561e015e548e2961959d0707c342c61d5097ea
Olha Pokolenko <olhp@ciklum.com>
2023-01-05 16:03:03 +0200

    AddContactWithDialog

---
+++ contact-book-connected-server-$-with-dialog/index.js
@@ -0,0 +1,204 @@
+const EDIT_BTN_CLASS = 'editBtn';
+const DELETE_BTN_CLASS = 'deleteBtn';
+const CONTACT_ITEM_SELECTOR = '.contactItem';
+const FORM_DOM_EL = 0;
+const ADD_BUTTON_SELECTOR = '#addNewContact';
+const FORM = 'form';
+
+const $form = $('#contactForm');
+const $dialogForm = $("#dialog-form");
+const $inputs = $('.formInput');
+const $contactContainer = $('#contactContainer');
+let contactList = [];
+
+$form.on('click', ADD_BUTTON_SELECTOR, onAddContactBtnClick)
+$contactContainer
+  .on('click', '.' + DELETE_BTN_CLASS, onContactDeleteClick)
+  .on('click', '.' + EDIT_BTN_CLASS, onContactEditClick);
+
+const dialog = $dialogForm.dialog({
+  autoOpen: false,
+  height: 400,
+  width: 350,
+  modal: true,
+  buttons: {
+    Save: () => {
+      const contact = getUserInput(formDetails);
+
+      if (!isContactValid(contact)) {
+        showError(new Error('Wrong input data'));
+        return;
+      }
+
+      saveContact(contact);
+      renderContactList(contactList);
+    },
+    Cancel: function() {
+      dialog.dialog('close');
+    }
+  },
+  close: function() {
+    dialog.dialog('close');
+  }
+});
+
+const formDetails = dialog.find(FORM)[FORM_DOM_EL];
+
+init();
+
+function init() {
+  ContactApi.getList()
+    .then((list) => {
+      contactList = list;
+      renderContactList(list)
+    })
+    .catch(showError)
+}
+
+function onAddContactBtnClick(e) {
+  e.preventDefault();
+  dialog.dialog('open');
+}
+
+function onContactDeleteClick(event) {
+  const contactEl = findContactEl($(this));
+  const contact = findContactByContactEl(contactEl);
+
+  if (contact) {
+    setContactData(contactList.filter((el) => el.id != contact.id))
+    contactEl.remove();
+    ContactApi.delete(contact.id).then(() => {
+      alert('Contact was successfully deleted')
+   })
+  }
+}
+
+function onContactEditClick(e) {
+  const $contactEl = findContactEl($(this));
+  const contact = findContactByContactEl($contactEl)
+
+  if (contact) {
+      setFormData(contact)
+  }
+}
+
+function findContactEl($el) {
+  return $el.closest(CONTACT_ITEM_SELECTOR);
+}
+
+function findContactByContactEl($contactEl) {
+  const id = $contactEl.data('id');
+
+  return findContactById(String(id));
+}
+
+function setContactData(data) {
+  return (contactList = data);
+}
+
+function setFormData(contact) {
+  for (const input of $inputs) {
+    if (Object.hasOwn(contact, input.name)) {
+      input.value = contact[input.name];
+    }
+  }
+}
+
+function getUserInput(data) {
+  return {
+    firstName: data.firstName.value,
+    lastName: data.lastName.value,
+    phone: data.phone.value
+  }
+}
+
+function isContactValid(contact) {
+  return !isEmpty(contact.firstName)
+    && !isEmpty(contact.lastName)
+    && !isEmpty(contact.phone)
+    && isPhone(contact.phone);
+}
+
+function isEmpty(str) {
+  return str === '';
+}
+
+function isPhone(phone) {
+  return !isNaN(phone);
+}
+
+function saveContact(contact) {
+  if (contact.id) {
+    ContactApi.update(contact.id, contact)
+      .then((newContact) => {
+        updateKeys(contact.id, newContact);
+        dialogClose();
+      })
+      .catch(showError)
+
+    replaceContact(contact.id, contact);
+  } else {
+    ContactApi.create(contact)
+      .then((newContact) => {
+        contactList.push(newContact);
+        renderContact(newContact)
+        dialogClose();
+      })
+      .catch(showError)
+  }
+
+}
+
+function renderContactList(contacts) {
+  const htmlArr = contacts.map(generateHTML);
+
+  $contactContainer.html(htmlArr);
+}
+
+function renderContact(contact) {
+  const html = generateHTML(contact);
+
+  $contactContainer.append(html);
+}
+
+function dialogClose() {
+  formDetails.reset();
+  dialog.dialog("close");
+}
+
+function showError(error) {
+  alert(error.message)
+}
+
+function findContactById(id) {
+  return contactList.find(item => item.id === id);
+}
+
+function updateKeys(id, changes) {
+  const oldContact = findContactById(id)
+
+  Object.keys(changes).forEach(key => oldContact[key] = changes[key]);
+}
+
+function replaceContact(id, contact) {
+  const $oldTodoEl = $(`[data-id="${id}"]`);
+  const html = generateHTML(contact);
+
+  $oldTodoEl.replaceWith(html);
+}
+
+function generateHTML(contact) {
+  return `
+    <tr class="contactItem" data-id="${contact.id}">
+      <td>${contact.firstName}</td>
+      <td>${contact.lastName}</td>
+      <td>${contact.phone}</td>
+      <td>
+        <span>
+            <button type="button" class="editBtn">[Edit]</button>
+            <button type="button" class="deleteBtn">[Delete]</button>
+        </span>
+      </td>
+    </tr>
+  `;
+}
