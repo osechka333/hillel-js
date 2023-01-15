986afe7c08628f5a97355013d0fe21a5c56a0ee4
opokolenko <opokolenko@questrade.com>
2023-01-15 14:26:07 +0200

    fix

---
+++ todo-list-server-mvc-jquery/Controller.js
@@ -0,0 +1,40 @@
+class Controller {
+    constructor($root) {
+        this.collection = new Collection();
+        this.listView = new ListView({
+            onEdit: id => this.formView.setFormData(this.collection.get(id)),
+            onDelete: id => this.delete(id)
+        });
+        this.formView = new FormView({
+            onSubmit: (item) => this.save(item)
+        });
+
+        this.listView.appendTo($root);
+        this.formView.appendTo($root);
+
+        this.collection
+            .fetch()
+            .then((list) => {
+                this.listView.renderList(list);
+            })
+    }
+
+    save(item) {
+        this.collection
+            .save(item)
+            .then((newItem) => {
+                if(item.id) {
+                    this.listView.replaceItem(newItem.id, newItem);
+                } else {
+                    this.listView.renderItem(newItem);
+                }
+            });
+    }
+    delete(id) {
+        this.collection
+            .delete(id)
+            .then(()=>{
+                this.listView.remove(id);
+            })
+    }
+}
