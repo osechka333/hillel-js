6bb18f5263297214f56d71431e6cb3133df3196a
opokolenko <opokolenko@questrade.com>
2023-02-19 22:11:59 +0200

    updatedTodoList

--- todo-list-redux-thunk/src/store/index.js
+++ todo-list-redux-thunk/src/store/index.js
@@ -1,10 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
-import { rootReducer } from './reducers';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers'
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    // applyMiddleware(...middleware)
-    )
-);
+export const store  = createStore(rootReducer);
--- todo-list-redux-thunk/src/store/reducers/todo.js
+++ todo-list-redux-thunk/src/store/reducers/todo.js
@@ -1,6 +1,6 @@
 import {
   ACTION_CREATE,
-  ACTION_DELETE,
+  ACTION_DELETE, ACTION_STATUS_UPDATE,
   ACTION_UPDATE
 } from '../actions/todo'
 
@@ -10,10 +10,10 @@ const DEFAULT_ITEM_CONTENT = {
 }
 const initialState = {
   list: [
-    { 'title': 'item 1', 'status': true, 'done': true, 'id': '1' },
-    { 'title': 'item 2', 'status': true, 'done': false, 'id': '2' },
-    { 'title': 'item 3', 'status': true, 'done': true, 'id': '3' },
-    { 'title': 'item 4', 'status': true, 'done': false, 'id': '4' }
+    { title: 'item 1', done: true, id: '1' },
+    { title: 'item 2', done: false, id: '2' },
+    { title: 'item 3', done: true, id: '3' },
+    { title: 'item 4', done: false, id: '4' }
   ],
   editItem: DEFAULT_ITEM_CONTENT,
 }
@@ -33,6 +33,12 @@ export default function todoReducer (state = initialState, { type, payload }) {
       }
     case ACTION_DELETE: return { ...state, list: state.list.filter(item => item.id !== payload) }
     case ACTION_UPDATE: return { ...state, editItem: payload }
+    case ACTION_STATUS_UPDATE:
+      const updatedItem = state.list.find(item => item.id === payload)
+      const done = !(updatedItem.done);
+      updatedItem.done = done
+      return { ...state, list: state.list.map(todoItem => todoItem.id === payload ? updatedItem : todoItem)};
+
     default: return state;
   }
 }
